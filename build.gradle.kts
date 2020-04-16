2ce3dddb1b2b7adca2e6b32e34a769e3e63b1a23
alicefranco <github0137#>
2020-04-16 15:18:25 +0100

    Add buildSrc directory and convert build gradle scripts to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+        maven(url = "https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath(PluginDependencies.GRADLE)
+        classpath(PluginDependencies.KOTLIN)
+        classpath(PluginDependencies.KTLINT)
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = AppConfig.JVM_TARGET
+    }
+}
+
+tasks.register("clean").configure {
+    delete(rootProject.buildDir)
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+    const val APP_COMPAT = "1.1.0"
+    const val CORE_KTX = "1.2.0"
+    const val CONSTRAINT_LAYOUT = "1.1.3"
+
+    const val JUNIT = "4.12"
+    const val TEST_RUNNER = "1.2.0"
+    const val ESPRESSO = "3.2.0"
+}
